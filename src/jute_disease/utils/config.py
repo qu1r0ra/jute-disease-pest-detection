1e3175a284f5746efba17cd598485711cccbb04c
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2026-01-23 20:18:28 +0800

    feat(scripts): add config base

--- src/jute_disease/utils/config.py
+++ src/jute_disease/utils/config.py
@@ -1,1 +1,23 @@
+from pathlib import Path
 
+# Paths
+ROOT_DIR = Path(__file__).resolve(strict=True).parents[3]
+DATA_DIR = ROOT_DIR / "data"
+
+# Image Configuration
+IMAGE_SIZE = 1024
+BATCH_SIZE = 32  # NOTE: Not final.
+NUM_WORKERS = 4  # NOTE: Not final.
+
+# Training Configuration
+MAX_EPOCHS = 100  # NOTE: Not final.
+LEARNING_RATE = 1e-3  # NOTE: Not final.
+
+# Dataset Classes
+CLASSES = [
+    "Diehard",
+    "Holed",
+    "Mosaic",
+    "Stem Soft Rot",
+    "Fresh",
+]
