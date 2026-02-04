363d9f9989a629c658587ae524cdf024b1023e17
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2026-02-04 17:30:31 +0800

    feat: add dl backbone bases

---
+++ src/jute_disease_pest/models/dl_backbones/vgg.py
@@ -0,0 +1,17 @@
+import timm
+import torch
+from torch import nn
+
+
+class VGG(nn.Module):
+    def __init__(self, pretrained: bool = True):
+        super().__init__()
+        self.backbone = timm.create_model(
+            "vgg19",
+            pretrained=pretrained,
+            num_classes=0,
+        )
+        self.out_features = self.backbone.num_features
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.backbone(x)
