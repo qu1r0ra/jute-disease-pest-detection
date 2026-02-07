b9030e816421bb431efdcd45c1e416312a51a3ba
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2026-02-08 02:34:21 +0800

    fix: disable wandb for ci

--- src/jute_disease/utils/wandb_utils.py
+++ src/jute_disease/utils/wandb_utils.py
@@ -11,5 +11,8 @@ def setup_wandb():
     api_key = os.getenv("WANDB_API_KEY")
     if api_key:
         wandb.login(key=api_key)
+    elif os.getenv("WANDB_MODE") in ["disabled", "offline"]:
+        pass
     else:
+        # Only prompt/login if not explicitly disabled and no unexpected CI environment
         wandb.login()
