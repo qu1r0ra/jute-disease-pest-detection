3463b2669b24d48c9608ac3dcdd7681cc38d995e
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2026-02-24 01:38:47 +0800

    refactor: move database uri to env

--- src/annotator/__init__.py
+++ src/annotator/__init__.py
@@ -1,14 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
 from annotator.blueprints.analysis import analysis_bp
 from annotator.blueprints.annotation import annotation_bp
 from annotator.models import db
 
+load_dotenv()
+
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.update(
-        SQLALCHEMY_DATABASE_URI="sqlite:///annotations.db",
+        SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL", "sqlite:///annotations.db"),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
     db.init_app(app)
