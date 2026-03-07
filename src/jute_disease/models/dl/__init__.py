9070f1fa8aa97dd20f416f5fbb63cf433ac960ac
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2026-03-08 01:52:25 +0800

    chore: remove standalone mobilevit

--- src/jute_disease/models/dl/__init__.py
+++ src/jute_disease/models/dl/__init__.py
@@ -1,5 +1,4 @@
 from jute_disease.models.dl.backbone import TimmBackbone
 from jute_disease.models.dl.classifier import Classifier
-from jute_disease.models.dl.mobilevit import MobileViT
 
-__all__ = ["Classifier", "MobileViT", "TimmBackbone"]
+__all__ = ["Classifier", "TimmBackbone"]
